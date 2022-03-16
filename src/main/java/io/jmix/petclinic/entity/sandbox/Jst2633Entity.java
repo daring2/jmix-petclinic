459236721a4ea24a541b3a57703ae9b5c4b10fbb
daring <danila.daring@gmail.com>
2022-03-16 17:20:39 +0300

    introduce Jst2633Entity

---
+++ src/main/java/io/jmix/petclinic/entity/sandbox/Jst2633Entity.java
@@ -0,0 +1,13 @@
+package io.jmix.petclinic.entity.sandbox;
+
+import com.company.jmix_kotlin_addon.entity.StandardEntity;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@JmixEntity
+@Entity(name = "petclinic_Jst2633Entity")
+@Table(name = "PETCLINIC_JST2633_ENTITY")
+public class Jst2633Entity extends StandardEntity {
+}
