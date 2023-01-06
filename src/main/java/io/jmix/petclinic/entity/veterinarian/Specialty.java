5ef109977b2d8d0776d676c45e0df55a49b1586a
daring <danila.daring@gmail.com>
2023-01-06 20:30:06 +0300

    add drag-and-drop example

--- src/main/java/io/jmix/petclinic/entity/veterinarian/Specialty.java
+++ src/main/java/io/jmix/petclinic/entity/veterinarian/Specialty.java
@@ -1,13 +1,28 @@
 package io.jmix.petclinic.entity.veterinarian;
 
 import io.jmix.core.metamodel.annotation.JmixEntity;
+import io.jmix.core.metamodel.annotation.JmixProperty;
 import io.jmix.petclinic.entity.NamedEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @JmixEntity
 @Table(name = "PETCLINIC_SPECIALTY")
 @Entity(name = "petclinic_Specialty")
 public class Specialty extends NamedEntity {
+
+    @JmixProperty
+    @Transient
+    private Specialty parent;
+
+    public Specialty getParent() {
+        return parent;
+    }
+
+    public void setParent(Specialty parent) {
+        this.parent = parent;
+    }
+
 }
--- src/main/java/io/jmix/petclinic/screen/veterinarian/specialty/SpecialtyBrowse.java
+++ src/main/java/io/jmix/petclinic/screen/veterinarian/specialty/SpecialtyBrowse.java
@@ -1,15 +1,53 @@
 package io.jmix.petclinic.screen.veterinarian.specialty;
 
+import com.vaadin.shared.ui.dnd.DragSourceState;
+import com.vaadin.shared.ui.dnd.EffectAllowed;
+import com.vaadin.shared.ui.grid.DropMode;
+import com.vaadin.ui.components.grid.GridDragSource;
+import com.vaadin.ui.components.grid.TreeGridDropTarget;
 import io.jmix.petclinic.entity.veterinarian.Specialty;
+import io.jmix.ui.component.DataGrid;
+import io.jmix.ui.component.TreeDataGrid;
 import io.jmix.ui.navigation.Route;
-import io.jmix.ui.screen.LookupComponent;
-import io.jmix.ui.screen.MasterDetailScreen;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 @UiController("petclinic_Specialty.browse")
 @UiDescriptor("specialty-browse.xml")
 @LookupComponent("table")
 @Route(value = "specialities")
 public class SpecialtyBrowse extends MasterDetailScreen<Specialty> {
+
+    @Autowired
+    private DataGrid<Specialty> table;
+    @Autowired
+    private TreeDataGrid<Specialty> treeGrid;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        initDragAndDrop();
+    }
+
+    private void initDragAndDrop() {
+        var vaadinGrid = table.unwrap(com.vaadin.ui.Grid.class);
+        var dragSource = new GridDragSource<Specialty>(vaadinGrid);
+        dragSource.setEffectAllowed(EffectAllowed.LINK);
+        dragSource.setDragDataGenerator(DragSourceState.DATA_TYPE_TEXT_PLAIN, item -> item.getId().toString());
+        var vaadinTreeGrid = treeGrid.unwrap(com.vaadin.ui.TreeGrid.class);
+        var dropTarget = new TreeGridDropTarget<Specialty>(vaadinTreeGrid, DropMode.ON_TOP);
+        dropTarget.addTreeGridDropListener(event -> {
+            String dragItem = event.getDataTransferData(DragSourceState.DATA_TYPE_TEXT_PLAIN)
+                    .map(UUID::fromString)
+                    .map(id -> table.getItems().getItem(id))
+                    .map(Specialty::getName)
+                    .orElse(null);
+            String dropItem = event.getDropTargetRow()
+                    .map(Specialty::getName)
+                    .orElse(null);
+            System.out.println("dragItem=" + dragItem + ", dropItemId=" + dropItem);
+        });
+    }
+
 }
